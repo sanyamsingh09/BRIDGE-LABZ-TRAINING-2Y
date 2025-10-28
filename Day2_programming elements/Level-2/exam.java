e48c1f684002c531b08f3f0a312602dbad7baa9f
sanyamsingh <sanyamsingh377@gmail.com>
2025-10-28 14:53:12 +0530

    Delete Day2_programming elements directory

--- Day2_programming elements/Level-2/exam.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Student Registration System {
-    public static void  main(String[] args) {
-        system.out.println("student names");
-        
-    }
-    public static void ()
-
-}
